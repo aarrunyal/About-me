37c216b0a045bbd141571fe9dcfd3e21e1e4bfff
Arun Aryal <39483492+aarrunyal@users.noreply.github.com>
2023-10-02 10:54:33 -0400

    Create index.js
    About me added

---
+++ index.js
@@ -0,0 +1,10 @@
+   const i = {
+        am : "Full Stack Software Developer",
+        worked_at: [
+                "Australian Music Examination Board", 
+                "Yelko Web Pvt. Ltd.", 
+                "Goglides Group Pvt. Ltd"
+            ],
+        good_at : ["Java", "Spring", "Spring Boot", "PHP", "Laravel", "Angular", "Vue.js", "GIT", "Python"],
+        love_to : ["Reading Book", "Cycling", "Traveling", "Watching Sports", ]
+    }
