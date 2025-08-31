7cc1b24608ede3a9b7d1c16b60c767e455ea4c99
Arun Aryal <39483492+aarrunyal@users.noreply.github.com>
2025-08-31 14:51:51 -0400

    Update employment and skills information

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
    const i = {
         am : "Full Stack Software Developer",
-        working_at: "Disguise Technology", 
+        working_at: "Disguise System Canada Inc.", 
         worked_at: [
                 "Worldview Internation Services Inc.",
                 "Australian Music Examination Board", 
                 "Yelko Web Pvt. Ltd.", 
                 "Goglides Group Pvt. Ltd"
             ],
-        good_at : ["Java", "Spring", "Spring Boot", "PHP", "Laravel", "Angular", "Vue.js", "GIT", "Python"],
+        good_at : ["PHP", "Laravel", "Angular", "Vue.js", "Python", "Django", "Docker"],
         love_to : ["Reading Book", "Cycling", "Traveling", "Watching Sports", ]
     }
